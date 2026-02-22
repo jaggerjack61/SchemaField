bf915b1f8d894bf6d9d760204b9cb2e0349c0cb7
samuel jarai <jarai.samuel@gmail.com>
2026-02-22 19:36:48 +0200

    up dated git helper

--- g.bat
+++ g.bat
@@ -5,6 +5,22 @@ if "%~1"=="" (
     exit /b
 )
 
+:: If -p is provided, run git pull
+if "%~1"=="-p" (
+    git pull
+    exit /b
+)
+
+:: If -b <branch_name> is provided, checkout branch
+if "%~1"=="-b" (
+    if "%~2"=="" (
+        echo Usage: g.bat -b ^<branch_name^>
+        exit /b 1
+    )
+    git checkout "%~2"
+    exit /b
+)
+
 :: If an argument exists, run the add-commit-push sequence
 git add .
 git commit -m "%~1"
