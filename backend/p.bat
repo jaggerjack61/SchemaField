8923d7f3dd301b25b52539d66fed287ce2c79268
samuel jarai <jarai.samuel@gmail.com>
2026-02-23 00:04:25 +0200

    added help to reduce keystrokes during dev

---
+++ backend/p.bat
@@ -0,0 +1,61 @@
+@echo off
+
+if "%~1"=="" (
+    python manage.py runserver
+    exit /b
+)
+
+if "%~1"=="-a" (
+    call ".\venv\Scripts\activate"
+    exit /b
+)
+
+if "%~1"=="-m" (
+    if "%~2"=="-r" (
+        echo Running migrate refresh
+        @REM delete all sqlite3 files
+        del /q /s "db.sqlite3"
+        python manage.py migrate
+        exit /b
+      
+    )
+    python manage.py migrate
+    exit /b
+)
+
+if "%~1"=="-cs" (
+    python manage.py createsuperuser
+    exit /b
+    
+)
+
+if "%~1"=="-c" (
+    python manage.py check
+    exit /b
+    
+)
+
+if "%~1"=="-h" (
+    echo Usage: 
+    echo p ^<port^>: Run the server on the specified port. Port is optional
+    echo p -a:       Activate virtual environment
+    echo p -m:       Run database migrations
+    echo p -m -r:    Refresh database migrations
+    echo p -cs:      Create superuser
+    echo p -h:       Show this help message
+    exit /b
+)
+
+if not "%~1"=="" (
+    if not "%~2"=="" (
+        echo Usage: p ^<port^>
+        exit /b 1
+    )
+    python manage.py runserver "%~1"
+    exit /b
+)
+
+
+
+
+
--- g.bat
+++ g.bat
@@ -5,6 +5,11 @@ if "%~1"=="" (
     exit /b
 )
 
+if "%~1"=="-i" (
+    git init
+    exit /b
+)
+
 :: If -p is provided, run git pull
 if "%~1"=="-p" (
     git pull
@@ -21,7 +26,25 @@ if "%~1"=="-b" (
     exit /b
 )
 
+if "%~1"=="-h" (
+    echo Git Helper 
+    echo -i: Initialize a new Git repository
+    echo -p: Pull the latest changes from the remote repository
+    echo -b ^<branch_name^>: Checkout the specified branch
+    echo ^<commit_message^>: Add, commit, and push changes with the provided message
+    exit /b
+)
+
+if not "%~1"=="" (
+    if not "%~2"=="" (
+        echo Only one argument is expected
+        exit /b 1
+    )
+    git add .
+    git commit -m "%~1"
+    git push
+    exit /b
+)
+
 :: If an argument exists, run the add-commit-push sequence
-git add .
-git commit -m "%~1"
-git push
+
